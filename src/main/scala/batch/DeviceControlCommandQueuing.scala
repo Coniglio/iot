8122484a286e3c23acd3093d0b9ea36ee86cc7d4
tts <tts@ymsht.info>
2019-03-24 20:46:49 +0900

    機器制御クラス追加

---
+++ src/main/scala/batch/DeviceControlCommandQueuing.scala
@@ -0,0 +1,24 @@
+package batch
+
+import batch.LightsUpInTheMorning.args
+import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
+
+/**
+  * 機器制御用コマンドをキューイング
+  */
+class DeviceControlCommandQueuing extends App {
+
+  val queueName = args(0)
+  val command = args(1)
+
+  val factory = new ConnectionFactory
+  factory.setHost("localhost")
+  val connection = factory.newConnection
+  val channel = connection.createChannel
+
+  channel.queueDeclare(queueName, false, false, false, null)
+  channel.basicPublish("", queueName, null, command.getBytes)
+
+  channel.close
+  connection.close
+}
